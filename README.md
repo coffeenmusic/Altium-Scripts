201679539532f6be10f537f3df26b99a20cb482f
Stephen Thompson <Stephen.Revel.Thompson@gmail.com>
2025-04-17 11:06:36 -0400

    Calculate current for a track's full signal on a given layer

---
+++ README.md
@@ -0,0 +1,3 @@
+# References
+- Used Current Calculator script by John Michael Go-Soco (ETL Systems Ltd)
+- Used the SelectBadConnections script by Petar Perisin as a reference to walk tracks
--- export_net_currents.pas
+++ export_net_currents.pas
@@ -1,8 +1,9 @@
 {..............................................................................}
-{ Net Current Capacity Analyzer                                               }
+{ Net Current Capacity Analyzer - Complete Path Version                       }
 {                                                                             }
 { Analyzes all nets on a PCB and generates a CSV file with                    }
 { net names and their worst-case current capacities                           }
+{ Calculates current capacity based on complete paths with the same width     }
 {..............................................................................}
 
 // Power function from the original script
@@ -21,8 +22,8 @@ Begin
         Result := Value2;
 End;
 
-// Helper function to calculate current capacity for a track/arc
-Function CalculateCurrentCapacity(Track : IPCB_Primitive; Board : IPCB_Board; LStack : IPCB_LayerStack) : Double;
+// Helper function to calculate current capacity for a track path
+Function CalculatePathCurrentCapacity(Tracks : TObjectList; Board : IPCB_Board; LStack : IPCB_LayerStack) : Double;
 Var
     h : Double; // Layer Thickness in mils
     w : Double; // Track Width in mils
@@ -32,8 +33,18 @@ Var
     isMidLayer : Boolean;
     I_10 : Double; // Current for 10°C rise (we'll use this as our reference)
     tk_10 : Double; // Temperature rise constant
+    TotalLength : Double; // Total path length in mils
+    Track : IPCB_Primitive;
+    TrackLength : Double;
+    i : Integer;
 Begin
     Result := 0; // Initialize with 0
+
+    // No tracks in path
+    if (Tracks = nil) or (Tracks.Count = 0) then Exit;
+
+    // Get the first track to determine width and layer
+    Track := Tracks[0];
 
     // Get layer information
     L1 := LStack.LayerObject(Track.Layer);
@@ -42,7 +53,7 @@ Begin
     // Get Layer Thickness in mils
     h := coordToMils(L1.CopperThickness);
 
-    // Determine width based on object type
+    // Determine width based on object type of first track
     Case Track.ObjectID of
         eTrackObject:
             w := coordToMils(Track.Width);
@@ -54,6 +65,25 @@ Begin
 
     // Skip if width or thickness is zero
     if (w <= 0) or (h <= 0) then Exit;
+
+    // Calculate total path length
+    TotalLength := 0;
+    for i := 0 to Tracks.Count - 1 do
+    begin
+        Track := Tracks[i];
+
+        // Calculate length based on object type
+        Case Track.ObjectID of
+            eTrackObject:
+                TrackLength := coordToMils(sqrt(sqr(Track.x2 - Track.x1) + sqr(Track.y2 - Track.Y1)));
+            eArcObject:
+                TrackLength := coordToMils(((Track.StartAngle - Track.EndAngle)/360) * pi * Track.Radius);
+            else
+                TrackLength := 0;
+        End;
+
+        TotalLength := TotalLength + TrackLength;
+    end;
 
     // Assign IPC-2221 constants
     c := 0.725;
@@ -80,6 +110,176 @@ Begin
     Result := I_10;
 End;
 
+// Helper function to get the key for a track (used for identifying unique paths)
+Function GetTrackKey(Track : IPCB_Primitive) : String;
+Begin
+    // Create a unique key based on track properties
+    Result := IntToStr(Track.Layer) + '-';
+
+    Case Track.ObjectID of
+        eTrackObject:
+            Result := Result + IntToStr(Track.Width);
+        eArcObject:
+            Result := Result + IntToStr(Track.LineWidth);
+        else
+            Result := Result + '0';
+    End;
+End;
+
+// Helper function to find connected tracks at a point
+Procedure FindConnectedTracks(Board : IPCB_Board; Net : IPCB_Net; X, Y : Integer;
+                              Layer : TLayer; Width : TCoord; ProcessedTracks : TObjectList;
+                              CurrentPath : TObjectList; Tolerance : TCoord);
+Var
+    SIter : IPCB_SpatialIterator;
+    Track : IPCB_Primitive;
+    ConnectedX, ConnectedY : Integer;
+    IsConnected : Boolean;
+Begin
+    // Create spatial iterator to find nearby objects
+    SIter := Board.SpatialIterator_Create;
+    SIter.AddFilter_ObjectSet(MkSet(eTrackObject, eArcObject));
+    SIter.AddFilter_LayerSet(MkSet(Layer));
+    SIter.AddFilter_Area(X - Tolerance, Y - Tolerance, X + Tolerance, Y + Tolerance);
+
+    // Find all connected tracks
+    Track := SIter.FirstPCBObject;
+    While (Track <> nil) do
+    Begin
+        // Check if the track is in the same net and has the same width
+        if (Track.InNet) and (Track.Net.Name = Net.Name) and
+           (((Track.ObjectID = eTrackObject) and (Track.Width = Width)) or
+            ((Track.ObjectID = eArcObject) and (Track.LineWidth = Width))) then
+        begin
+            // Check if this track is already processed
+            if ProcessedTracks.IndexOf(Track) = -1 then
+            begin
+                // Determine connection point based on object type
+                IsConnected := False;
+
+                Case Track.ObjectID of
+                    eTrackObject:
+                    begin
+                        // Check if either endpoint connects
+                        if (Abs(Track.x1 - X) <= Tolerance) and (Abs(Track.y1 - Y) <= Tolerance) then
+                        begin
+                            ConnectedX := Track.x2;
+                            ConnectedY := Track.y2;
+                            IsConnected := True;
+                        end
+                        else if (Abs(Track.x2 - X) <= Tolerance) and (Abs(Track.y2 - Y) <= Tolerance) then
+                        begin
+                            ConnectedX := Track.x1;
+                            ConnectedY := Track.y1;
+                            IsConnected := True;
+                        end;
+                    end;
+                    eArcObject:
+                    begin
+                        // Check if either endpoint connects
+                        if (Abs(Track.StartX - X) <= Tolerance) and (Abs(Track.StartY - Y) <= Tolerance) then
+                        begin
+                            ConnectedX := Track.EndX;
+                            ConnectedY := Track.EndY;
+                            IsConnected := True;
+                        end
+                        else if (Abs(Track.EndX - X) <= Tolerance) and (Abs(Track.EndY - Y) <= Tolerance) then
+                        begin
+                            ConnectedX := Track.StartX;
+                            ConnectedY := Track.StartY;
+                            IsConnected := True;
+                        end;
+                    end;
+                end;
+
+                // If connected, add to path and continue tracing
+                if IsConnected then
+                begin
+                    // Add to processed list and current path
+                    ProcessedTracks.Add(Track);
+                    CurrentPath.Add(Track);
+
+                    // Recursively find next connected track
+                    FindConnectedTracks(Board, Net, ConnectedX, ConnectedY, Layer, Width,
+                                       ProcessedTracks, CurrentPath, Tolerance);
+                end;
+            end;
+        end;
+
+        Track := SIter.NextPCBObject;
+    End;
+
+    // Clean up
+    Board.SpatialIterator_Destroy(SIter);
+End;
+
+// Helper function to trace a complete path of connected tracks
+Function TraceCompletePath(Board : IPCB_Board; StartTrack : IPCB_Primitive;
+                          ProcessedTracks : TObjectList) : TObjectList;
+Var
+    CurrentPath : TObjectList;
+    X1, Y1, X2, Y2, i : Integer;
+    Width : TCoord;
+    Layer : TLayer;
+    Tolerance : TCoord;
+    Obj : TObject;
+Begin
+    // Create object list for the current path
+    CurrentPath := CreateObject(TObjectList);
+    CurrentPath.OwnsObjects := False; // Don't destroy track objects
+
+    // Add starting track to path and processed list
+    CurrentPath.Add(StartTrack);
+    ProcessedTracks.Add(StartTrack);
+
+    // Get track properties
+    Layer := StartTrack.Layer;
+
+    Case StartTrack.ObjectID of
+        eTrackObject:
+        begin
+            Width := StartTrack.Width;
+            X1 := StartTrack.x1;
+            Y1 := StartTrack.y1;
+            X2 := StartTrack.x2;
+            Y2 := StartTrack.y2;
+        end;
+        eArcObject:
+        begin
+            Width := StartTrack.LineWidth;
+            X1 := StartTrack.StartX;
+            Y1 := StartTrack.StartY;
+            X2 := StartTrack.EndX;
+            Y2 := StartTrack.EndY;
+        end;
+        else
+        begin
+            // Not a track or arc
+            Result := CurrentPath;
+            Exit;
+        end;
+    End;
+
+    // Set tolerance for connection detection (1 mil)
+    Tolerance := MilsToCoord(1);
+
+    // Trace in both directions from start track
+    FindConnectedTracks(Board, StartTrack.Net, X1, Y1, Layer, Width, ProcessedTracks, CurrentPath, Tolerance);
+    FindConnectedTracks(Board, StartTrack.Net, X2, Y2, Layer, Width, ProcessedTracks, CurrentPath, Tolerance);
+
+    // Test: View selected Path
+    //For i := 0 to CurrentPath.Count - 1 Do
+    //Begin
+    //    Obj := CurrentPath[i];
+    //    Obj.Selected := True;
+    //End;
+
+    //Client.SendMessage('PCB:Zoom', 'Action=Selected' , 255, Client.CurrentView);
+    //Client.SendMessage('PCB:DeSelect', 'Scope=All', 255, Client.CurrentView); // Deselect All
+
+    Result := CurrentPath;
+End;
+
 // Main procedure to analyze net current capacities and save to CSV
 Procedure AnalyzeNetCurrentCapacities;
 Var
@@ -88,13 +288,16 @@ Var
     NetList : TObjectList;
     NetIterator : IPCB_BoardIterator;
     CurrentNet : IPCB_Net;
+    TrackIter : IPCB_GroupIterator;
+    Track : IPCB_Primitive;
+    ProcessedTracks : TObjectList;
+    CurrentPath : TObjectList;
+    PathCapacity : Double;
     i : Integer;
-    GrpIter : IPCB_GroupIterator;
-    TrackObj : IPCB_Primitive;
-    CurrentCapacity, TrackCapacity : Double;
     ResultsList : TStringList;
-    HasTracks : Boolean;
-    NetName: String;
+    NetCapacities : TStringList;
+    MinNetCapacity : Double;
+    HasPaths : Boolean;
 Begin
     // Retrieve the current board
     Board := PCBServer.GetCurrentPCBBoard;
@@ -111,8 +314,15 @@ Begin
     NetList := CreateObject(TObjectList);
     NetList.OwnsObjects := False; // Don't destroy nets when list is freed
 
+    // Create list to track processed tracks
+    ProcessedTracks := CreateObject(TObjectList);
+    ProcessedTracks.OwnsObjects := False; // Don't destroy tracks
+
     // Create string list for results
     ResultsList := TStringList.Create;
+
+    // Create string list for net capacities
+    NetCapacities := TStringList.Create;
 
     Try
         // Add CSV header
@@ -129,6 +339,7 @@ Begin
         While (CurrentNet <> Nil) Do
         Begin
             NetList.Add(CurrentNet);
+            NetCapacities.Add('999999.9'); // Initialize with a high value
             CurrentNet := NetIterator.NextPCBObject;
         End;
 
@@ -140,51 +351,62 @@ Begin
         For i := 0 to NetList.Count - 1 Do
         Begin
             CurrentNet := NetList[i];
+            MinNetCapacity := 999999.9;
+            HasPaths := False;
 
-            If CurrentNet.Name = 'ENET-NC2' Then
+            // Create group iterator for this net's tracks
+            TrackIter := CurrentNet.GroupIterator_Create;
+            TrackIter.AddFilter_ObjectSet(MkSet(eTrackObject, eArcObject));
+
+            // Process each track that hasn't been processed yet
+            Track := TrackIter.FirstPCBObject;
+            While (Track <> Nil) Do
             Begin
-                 NetName := CurrentNet.Name;
-            End;
-
-            // Create group iterator for this net
-            GrpIter := CurrentNet.GroupIterator_Create;
-            GrpIter.AddFilter_ObjectSet(MkSet(eTrackObject, eArcObject));
-
-            // Start with a high value for worst case
-            CurrentCapacity := 999999.9; // Force double
-            HasTracks := False;
-
-            // Check all tracks/arcs in this net
-            TrackObj := GrpIter.FirstPCBObject;
-            While (TrackObj <> Nil) Do
-            Begin
-                // Calculate current capacity for this track
-                TrackCapacity := CalculateCurrentCapacity(TrackObj, Board, LStack);
-
-                // Only consider tracks with valid capacities (> 0)
-                if TrackCapacity > 0 then
+                // Only process tracks we haven't seen yet
+                if ProcessedTracks.IndexOf(Track) = -1 then
                 begin
-                    // Use our custom MinDouble function instead of Min
-                    CurrentCapacity := MinDouble(CurrentCapacity, TrackCapacity);
-                    HasTracks := True;
+                    // Trace the complete path starting from this track
+                    CurrentPath := TraceCompletePath(Board, Track, ProcessedTracks);
+
+                    // Calculate capacity for this path
+                    if CurrentPath.Count > 0 then
+                    begin
+                        PathCapacity := CalculatePathCurrentCapacity(CurrentPath, Board, LStack);
+
+                        // If valid capacity, update minimum
+                        if PathCapacity > 0 then
+                        begin
+                            MinNetCapacity := MinDouble(MinNetCapacity, PathCapacity);
+                            HasPaths := True;
+                        end;
+                    end;
+
+                    // Free the path list (not the objects)
+                    //CurrentPath.Free;
                 end;
 
-                TrackObj := GrpIter.NextPCBObject;
+                Track := TrackIter.NextPCBObject;
             End;
 
-            // Add to results if tracks were found with valid capacities
-            if HasTracks and (CurrentCapacity < 999999.0) then
-            begin
-                ResultsList.Add(CurrentNet.Name + ',' + FloatToStrF(CurrentCapacity, ffFixed, 10, 4));
-            end
-            else
-            begin
-                // No tracks in this net or all had zero capacity
-                ResultsList.Add(CurrentNet.Name + ',N/A');
-            end;
-
             // Destroy iterator
-            CurrentNet.GroupIterator_Destroy(GrpIter);
+            CurrentNet.GroupIterator_Destroy(TrackIter);
+
+            // Add result to list
+            if HasPaths and (MinNetCapacity < 999999.0) then
+            begin
+                NetCapacities[i] := FloatToStrF(MinNetCapacity, ffFixed, 10, 4);
+            end;
+        End;
+
+        // Create the final CSV with results
+        For i := 0 to NetList.Count - 1 Do
+        Begin
+            CurrentNet := NetList[i];
+
+            if NetCapacities[i] = '999999.9' then
+                ResultsList.Add(CurrentNet.Name + ',N/A')
+            else
+                ResultsList.Add(CurrentNet.Name + ',' + NetCapacities[i]);
         End;
 
         // Save results to CSV file
@@ -193,7 +415,9 @@ Begin
         ShowMessage('Analysis complete. Results saved to C:\Users\Stephen Thompson\Downloads\NetCurrents.csv');
 
     Finally
-        //NetList.Free; // Freeing TObjectList causes access error violation
+        // NetList.Free; -- Don't free this to avoid access violation
+        //ProcessedTracks.Free;
         ResultsList.Free;
+        NetCapacities.Free;
     End;
 End;
